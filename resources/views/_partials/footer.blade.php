db405aee3f42974508696e0fb013cbd92b84a8a1
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-05 14:39:34 +0100

    Add head and calender section

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,4 +1,4 @@
-    <footer class="flex flex-row justify-center w-screen mt-8 bg-indigo-600 ">
+    <footer class="flex flex-row justify-center text-white w-screen mt-8 bg-indigo-600 ">
         <!-- Your footer content goes here -->
         <p>&copy; {{ date('Y') }} Your Website</p>
     </footer>
--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,6 +1,6 @@
     <header>
         <nav class="w-screen justify-center align-middle p-10 border-b-4 border-indigo-600">
-            <ul class="w-screen flex flex-row text 3xl justify-evenly">
+            <ul class="w-screen flex flex-row text-2xl justify-evenly">
                 <li><a href="{{ route("home") }}">Home</a></li>
                 <li><a href="{{ route("Reservations") }}">Reservations</a></li>
                 <li><a href="{{ route("MakeReservations") }}">Make Reservation</a></li>
