fd113fb36e77f1953ab33256030601c314a1ce48
Kilian Boute <kilian.boute@gmail.com>
2024-02-05 15:47:34 +0100

    Store reservation in database

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -15,4 +15,18 @@ class ReservationController extends Controller
             "reservations" => $reservation,
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'first_name' => ['required', 'max:255'],
+            'last_name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'number_of_people' => ['required', 'numeric', 'between:1,8']
+        ]);
+
+        Reservation::create($request->all());
+
+        return redirect('/')->with('success', 'Reservation created successfully');
+    }
 }
--- resources/views/reservations/MakeReservations.blade.php
+++ resources/views/reservations/MakeReservations.blade.php
@@ -1,57 +1,64 @@
 @extends('layout')
 
 @section('content')
-@include('_partials.header')
+    @include('_partials.header')
 
 @section('title')
-Sign Up
+    Sign Up
 @endsection
 
 @section('content')
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Reservation Form</h2>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Reservation Form</h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" action="#" method="POST">
+                @csrf
+                <div>
+                    <label for="first_name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+                    <div class="mt-2">
+                        <input id="first_name" name="first_name" type="text" autocomplete="given-name" required
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div>
+                    <label for="last_name" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
+                    <div class="mt-2">
+                        <input id="last_name" name="last_name" type="text" autocomplete="family-name" required
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
+                    <div class="mt-2">
+                        <input id="email" name="email" type="email" autocomplete="email" required
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div>
+                    <label for="number_of_people" class="block text-sm font-medium leading-6 text-gray-900">Number of
+                        People</label>
+                    <div class="mt-2">
+                        <input id="number_of_people" name="number_of_people" type="number" min="1" required
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit
+                        Reservation</button>
+                </div>
+            </form>
+        </div>
     </div>
-  
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
-        <div>
-          <label for="first_name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
-          <div class="mt-2">
-            <input id="first_name" name="first_name" type="text" autocomplete="given-name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-  
-        <div>
-          <label for="last_name" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
-          <div class="mt-2">
-            <input id="last_name" name="last_name" type="text" autocomplete="family-name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-  
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
-          <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-  
-        <div>
-          <label for="number_of_people" class="block text-sm font-medium leading-6 text-gray-900">Number of People</label>
-          <div class="mt-2">
-            <input id="number_of_people" name="number_of_people" type="number" min="1" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-  
-        <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit Reservation</button>
-        </div>
-      </form>
-    </div>
-  </div>
 @endsection
 
 @include('_partials.footer')
-    <h1>Welcome,  Lets make a reservations!</h1>
-
+<h1>Welcome, Lets make a reservations!</h1>
 @endsection
