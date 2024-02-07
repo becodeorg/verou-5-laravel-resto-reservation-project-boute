1e0bc66efaecdd0b740d7511d62420686044382e
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-07 14:09:17 +0100

    Show confirmation window after clicking a date and hour for event

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,30 @@
 @section('content')
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const calendarEl = document.getElementById('calendar');
-            const calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'timeGridWeek', // weekly view
-                slotMinTime: '8:00:00', // slot max time and min time
-                slotMaxTime: '19:00:00',
-                events: <?php echo json_encode($events); ?>,
-                dateClick: function (info) {
-                    // Handle Date click event (e.g)
-                    const selectedDate = info.dateStr;
-                    console.log('Clicked on: ' + info.dateStr);
-                    // Add logic to handle reservation here 
-                    // Redirect to the reservation form with the selected date
-                    window.location.href = '/reservations/MakeReservations?date=' + selectedDate;
-                }
-            });
-            calendar.render();
-        });
+document.addEventListener('DOMContentLoaded', () => {
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'timeGridWeek', // weekly view
+        slotMinTime: '8:00:00', // slot max time and min time
+        slotMaxTime: '19:00:00',
+        events: <?php echo json_encode($events); ?>,
+        dateClick: (info) => {
+            // Handle Date click event
+            const selectedDate = info.dateStr;
+            console.log(`Clicked on: ${selectedDate}`);
+            
+            // Add logic to handle reservation here
+            const confirmed = confirm(`Do you want to make a reservation for ${selectedDate}?`);
+            
+            if (confirmed) {
+                // Redirect to the reservation form with the selected date
+                window.location.href = `/reservations/MakeReservations?date=${selectedDate}`;
+            }
+        }
+    });
+    calendar.render();
+});
+
     </script>
 
     <main class="w-screen flex flex-col justify-even items-center h-screen">
