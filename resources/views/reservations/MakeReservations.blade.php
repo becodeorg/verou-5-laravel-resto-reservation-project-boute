3e4732ab46dba1ea26c8d18e4b3856a6a58b1584
Kilian Boute <kilian.boute@gmail.com>
2024-02-09 16:33:02 +0100

    Calendar font color, remove all-day option

--- resources/views/reservations/MakeReservations.blade.php
+++ resources/views/reservations/MakeReservations.blade.php
@@ -14,6 +14,7 @@
             initialView: 'timeGridWeek',
             slotMinTime: '8:00:00',
             slotMaxTime: '19:00:00',
+            allDaySlot: false,
             events: <?php echo json_encode($events); ?>,
             dateClick: function(info) {
                 // Get the start and end times from the clicked date
@@ -39,11 +40,10 @@
 </script>
 
 <style>
-
     #form-calender {
-    width: 100vw;
-    display: flex;
-    flex-direction: row;
+        width: 100vw;
+        display: flex;
+        flex-direction: row;
     }
 </style>
 
@@ -56,66 +56,72 @@
             </div>
         </section>
 
-        
+
 
         <section id="form-calender" class="flex-row-reverse w-screen justify-center rounded-xl px-5 py-5 ">
-                <section class="mr-12">
-                    <div id="calendar-container" class="w-full md:w-full text-white p-5   mx-auto mt-6"></div> <!-- Adjust the width here -->
-                    <div id="time-picked" class="text-white"></>
-                </section>
-                <div>
-                    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <section class="mr-12">
+                <div id="calendar-container" class="w-full md:w-full text-white p-5   mx-auto mt-6"></div>
+                <!-- Adjust the width here -->
+                <div id="time-picked" class="text-white">
+                    </>
+            </section>
+            <div>
+                <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form class="space-y-10 w-96 ml-12" action="{{ route('reservation-store') }}" method="POST">
-                    @csrf
-                    <input type="hidden" id="start_time" name="start_time">
-                <div>
-                    <label for="first_name" class="block text-sm font-medium leading-6 text-orange-900">First Name</label>
-                    <div class="mt-2">
-                        <input id="first_name" name="first_name" type="text" autocomplete="given-name" required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                    </div>
+                        @csrf
+                        <input type="hidden" id="start_time" name="start_time">
+                        <div>
+                            <label for="first_name" class="block text-sm font-medium leading-6 text-orange-900">First
+                                Name</label>
+                            <div class="mt-2">
+                                <input id="first_name" name="first_name" type="text" autocomplete="given-name" required
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+
+                        <div>
+                            <label for="last_name" class="block text-sm font-medium leading-6 text-orange-900">Last
+                                Name</label>
+                            <div class="mt-2">
+                                <input id="last_name" name="last_name" type="text" autocomplete="family-name" required
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+
+                        <div>
+                            <label for="email" class="block text-sm font-medium leading-6 text-orange-900">Email
+                                Address</label>
+                            <div class="mt-2">
+                                <input id="email" name="email" type="email" autocomplete="email" required
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+
+                        <div>
+                            <label for="number_of_guests" class="block text-sm font-medium leading-6 text-orange-900">Number
+                                of
+                                People</label>
+                            <div class="mt-2">
+                                <input id="number_of_guests" name="number_of_guests" type="number" min="1" required
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+
+                        <div>
+                            <button type="submit"
+                                class="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit
+                                Reservation</button>
+                        </div>
+                    </form>
+
+
+                    @if (isset($date))
+                        <h1 class="text-center text-2xl mt-6 font-bold leading-9 tracking-tight text-gray-900">
+                            Reservation for {{ $date }}
+                        </h1>
+                    @endif
                 </div>
-
-                <div>
-                    <label for="last_name" class="block text-sm font-medium leading-6 text-orange-900">Last Name</label>
-                    <div class="mt-2">
-                        <input id="last_name" name="last_name" type="text" autocomplete="family-name" required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                    </div>
-                </div>
-
-                <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-orange-900">Email Address</label>
-                    <div class="mt-2">
-                        <input id="email" name="email" type="email" autocomplete="email" required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                    </div>
-                </div>
-
-                <div>
-                    <label for="number_of_guests" class="block text-sm font-medium leading-6 text-orange-900">Number of
-                        People</label>
-                    <div class="mt-2">
-                        <input id="number_of_guests" name="number_of_guests" type="number" min="1" required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                    </div>
-                </div>
-
-                <div>
-                    <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit
-                        Reservation</button>
-                </div>
-                </form>
-
-
-                @if (isset($date))
-                    <h1 class="text-center text-2xl mt-6 font-bold leading-9 tracking-tight text-gray-900">
-                        Reservation for {{ $date }}
-                    </h1>
-                @endif
             </div>
-        </div>
         </section>
     </div>
 @endsection
